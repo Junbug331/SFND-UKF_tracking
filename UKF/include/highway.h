23b7415977e876c2c54d8784d1b97e3721a11976
HyoJun Jung <junbug331@gmail.com>
2022-10-07 10:25:20 +0900

    first commit

---
+++ UKF/include/highway.h
@@ -0,0 +1,35 @@
+/* \author Aaron Brown */
+// Handle logic for creating traffic on highway and animating it
+
+#include "render.h"
+#include "lidar.h"
+#include "tools.h"
+#include "ukf.h"
+
+class Highway
+{
+public:
+	std::vector<Car> traffic;
+	Car egoCar;
+	Tools tools;
+	bool pass = true;
+	std::vector<double> rmseThreshold = {0.30,0.16,0.95,0.70};
+	std::vector<double> rmseFailLog = {0.0,0.0,0.0,0.0};
+	Lidar* lidar;
+	
+	// Parameters 
+	// --------------------------------
+	// Set which cars to track with UKF
+	std::vector<bool> trackCars = {true,true,true};
+	// Visualize sensor measurements
+	bool visualize_lidar = true;
+	bool visualize_radar = true;
+	bool visualize_pcd = false;
+	// Predict path in the future using UKF
+	double projectedTime = 0;
+	int projectedSteps = 0;
+	// --------------------------------
+
+	Highway(pcl::visualization::PCLVisualizer::Ptr& viewer);
+    void stepHighway(double egoVelocity, long long timestamp, int frame_per_sec, pcl::visualization::PCLVisualizer::Ptr& viewer);
+};
